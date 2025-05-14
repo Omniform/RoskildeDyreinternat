e78be1c5cac8fe2ac510a464dd5e2c6eddaf0e07
TokeDit <Tokedit11@gmail.com>
2025-05-14 12:07:21 +0200

    Added, Dog, Cat and Fish classes, and made ToString() methods for all of them.

---
+++ LibDyreInternat/Animal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDyreInternat
+{
+
+    public enum Sex
+    {
+        male = 1,
+        female,
+        hermaphrodite
+    }
+
+    public abstract class Animal
+    {
+        private static int idnext = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public int BirthYear { get; set; }
+        public int Weight { get; set; }
+        public Sex Sex { get; set; }
+
+
+        
+
+        public Animal(string name, int birthYear, int weight, Sex sex)
+        {
+            Id = idnext++;
+            Name = name;
+            BirthYear = birthYear;
+            Weight = weight;
+            Sex = sex;
+        }
+
+        //public override string ToString()
+        //{
+        //    string sex = "";
+        //    switch (Sex)
+        //    {
+        //        case Sex.male:
+        //            sex = "Han";
+        //            break;
+        //        case Sex.female:
+        //            sex = "Hun";
+        //            break;
+        //        case Sex.hermaphrodite:
+        //            sex = "Tvekønnet";
+        //            break;
+        //    }
+
+        //    return $"Dyrets navn er: {Name}\nFødselsår: {BirthYear}\nWeight: {Weight}\nKøn: {sex}";
+
+        //}
+
+
+    }
+
+}
---
+++ LibDyreInternat/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDyreInternat
+{
+    public class Cat : Animal
+    {
+        public string Race { get; set; }
+        public bool IsChildFriendly { get; set; }
+        public string FoodPrefrences { get; set; }
+        public int ChipNumber { get; set; }
+
+        public Cat(string race, bool isChildFriendly, string foodPrefrences, int chipNumber, string name, int birthYear, int weight, Sex sex) : base(name, birthYear, weight, sex)
+        {
+            Race = race;
+            IsChildFriendly = isChildFriendly;
+            FoodPrefrences = foodPrefrences;
+            ChipNumber = chipNumber;
+        }
+        public override string ToString()
+        {
+            string sex = "";
+            switch (Sex)
+            {
+                case Sex.male:
+                    sex = "Han";
+                    break;
+                case Sex.female:
+                    sex = "Hun";
+                    break;
+                case Sex.hermaphrodite:
+                    sex = "Tvekønnet";
+                    break;
+            }
+            return $"Navn: {Name}\nFødselsår: {BirthYear}\nWeight: {Weight}Kg\nKøn: {sex}\nRace: {Race}\nBørnevenlig: {((IsChildFriendly) ? "Ja" : "Nej")}\nFoder: {FoodPrefrences}\nChipnummer: {ChipNumber}";
+
+        }
+    }
+}
