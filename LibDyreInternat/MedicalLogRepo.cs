13de9fd5144123b8123ccb867f9d7cea0c13d7f8
Lars <lav005@edu.zealand.dk>
2025-05-14 11:27:15 +0200

    Tilføjet MedicalLog + Repo Add() og ToString()

---
+++ LibDyreInternat/MedicalLogRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDyreInternat
+{
+    public static class MedicalLogRepo 
+    {
+        public static List<MedicalLog> medicalLogs = new List<MedicalLog>();
+        public static void Add(string description, DateTime dateTime, Animal animal, string nameOfDoctor)
+        {
+            medicalLogs.Add(new(description, dateTime, animal, nameOfDoctor));
+        }
+        public static string AllToString()
+        {
+            string s = "";
+            foreach (MedicalLog medicalLog in medicalLogs) { s += medicalLog.ToString() + "\n"; }
+            return s + "\n";
+        }
+    }
+}
