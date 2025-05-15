07599a2ed504937878fe689c40318970c1b7a36e
Elvie-Abarth <estibrusse18@gmail.com>
2025-05-15 13:04:16 +0200

    fixt strage error

--- LibDyreInternat/PersonRepo.cs
+++ LibDyreInternat/PersonRepo.cs
@@ -11,9 +11,9 @@ namespace LibDyreInternat
         // Add an new person in list.
         public static List<Person> AllPersons { get; private set; } = new List<Person>()
             {
-                new Person("Toke", "01-01-01", "Holte", "12345678", "Toke@toke.toke", Person.Acceslevel.admin),
-                new Person("Esti",  "18-05-97", "Jyllinge", "6556145", "esti@gmail.com", Person.Acceslevel.kunde),
-                new Person("Lars", "01-01-01",  "Husum", "45678912", "LarsLars@larslars.lars", Person.Acceslevel.admin),
+                new Person("Toke", "01-01-01", "Holte", "12345678", "Toke@toke.dk", Person.Acceslevel.admin),
+                new Person("Esti",  "18-05-97", "Jyllinge", "65561475", "esti@gmail.com", Person.Acceslevel.kunde),
+                new Person("Lars", "01-01-01",  "Husum", "45678912", "LarsLars@larslars.dk", Person.Acceslevel.admin),
                 new Person("Stefan", "01-01-01", "Denmark", "65465456", "stefan@stefan.dk", Person.Acceslevel.medlem)
             };
 
